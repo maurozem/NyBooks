10466a490554c90d527ea2fdb2a1a2e6bc5ec9d9
maurozem <maurozem@brq.com>
2020-07-03 16:16:36 -0300

    implementação do flipper e tratamento de erros

--- app/src/main/java/ms/zem/nybooksplus/presentation/books/BookViewModel.kt
+++ app/src/main/java/ms/zem/nybooksplus/presentation/books/BookViewModel.kt
@@ -3,6 +3,7 @@ package ms.zem.nybooksplus.presentation.books
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ms.zem.nybooksplus.R
 import ms.zem.nybooksplus.data.APIServices
 import ms.zem.nybooksplus.data.model.Book
 import ms.zem.nybooksplus.data.response.BookBodyResponse
@@ -15,6 +16,7 @@ class BookViewModel : ViewModel() {
 
     private val booksLiveData: MutableLiveData<List<Book>> = MutableLiveData()
     val books: LiveData<List<Book>> = booksLiveData
+    val flipperLiveData: MutableLiveData<Pair<Int, Int?>> = MutableLiveData()
 
     fun getBooks(){
         APIServices.service.getBooks().enqueue(object : Callback<BookBodyResponse>{
@@ -22,21 +24,35 @@ class BookViewModel : ViewModel() {
                 call: Call<BookBodyResponse>,
                 response: Response<BookBodyResponse>
             ) {
-                if (response.isSuccessful){
-                    val mybooks : MutableList<Book> = mutableListOf()
-                    response.body()?.let {
-                        for (result in it.bookResults){
-                            mybooks.add(result.toBook())
+                when {
+                    response.isSuccessful -> {
+                        val mybooks : MutableList<Book> = mutableListOf()
+                        response.body()?.let {
+                            for (result in it.bookResults){
+                                mybooks.add(result.toBook())
+                            }
                         }
+                        booksLiveData.value = mybooks
+                        flipperLiveData.value = Pair(FLIPPER_BOOKS, null)
                     }
-                    booksLiveData.value = mybooks
+                    response.code() == 401 -> {
+                        flipperLiveData.value = Pair(FLIPPER_ERROR, R.string.error401)
+                    }
+                    else -> {
+                        flipperLiveData.value = Pair(FLIPPER_ERROR, R.string.error400)
+                    }
                 }
             }
 
             override fun onFailure(call: Call<BookBodyResponse>, t: Throwable) {
-                TODO("Not yet implemented")
+                flipperLiveData.value = Pair(FLIPPER_ERROR, R.string.error500)
             }
         })
+    }
+
+    companion object{
+        private const val FLIPPER_BOOKS = 1
+        private const val FLIPPER_ERROR = 2
     }
 
 }
--- app/src/main/java/ms/zem/nybooksplus/presentation/books/BooksActivity.kt
+++ app/src/main/java/ms/zem/nybooksplus/presentation/books/BooksActivity.kt
@@ -6,11 +6,12 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.activity_books.recyclerMain
+import kotlinx.android.synthetic.main.activity_books.*
 import kotlinx.android.synthetic.main.toolbar.toolbar
 import ms.zem.nybooksplus.R
 import ms.zem.nybooksplus.presentation.base.BaseActivity
 import ms.zem.nybooksplus.presentation.details.BookDetailsActivity
+import kotlin.time.seconds
 
 class BooksActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +22,15 @@ class BooksActivity : BaseActivity() {
 
         val viewModel: BookViewModel = ViewModelProvider(this).get(BookViewModel::class.java)
 
+
+        viewModel.flipperLiveData.observe(this, Observer {
+            it?.let { pair ->
+                flipper.displayedChild = pair.first
+                pair.second?.let { idTexto ->
+                    textError.text = getString(idTexto)
+                }
+            }
+        })
         viewModel.books.observe(this, Observer {
             it?.let {
                 val dividerItemDetailsActivity = DividerItemDecoration(this@BooksActivity, DividerItemDecoration.VERTICAL)
@@ -41,6 +51,7 @@ class BooksActivity : BaseActivity() {
         })
         viewModel.getBooks()
     }
+
 
 
 
